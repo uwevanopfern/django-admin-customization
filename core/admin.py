47bea70bd32f45829cc4dbefa03e62ae122ec325
UweVanOpfern <uweaime@gmail.com>
2021-01-03 02:29:28 +0200

    first commit

---
+++ core/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+
+from core.models import Person, Course, Grade
+
+from django.urls import reverse
+from django.utils.http import urlencode
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "show_average")
+    search_fields = ("last_name__startswith",)
+
+    def show_average(self, obj):
+        from django.utils.html import format_html
+        from django.db.models import Avg
+
+        result = Grade.objects.filter(person=obj).aggregate(Avg("grade"))
+        return format_html("<b><i>{}</i></b>", result["grade__avg"])
+
+    show_average.short_description = "Average"
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "year", "view_students_link")
+    list_filter = (
+        "year",
+        "name",
+    )
+
+    def view_students_link(self, obj):
+        from django.utils.html import format_html
+
+        count = obj.person_set.count()
+        url = (
+            reverse("admin:core_person_changelist")
+            + "?"
+            + urlencode({"courses__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} Students</a>', url, count)
+
+    view_students_link.short_description = "Students"
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
---
+++ core/models.py
@@ -0,0 +1,40 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+    courses = models.ManyToManyField("Course", blank=True)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+    class Meta:
+        verbose_name_plural = "People"
+        ordering = ("last_name", "first_name")
+
+
+class Course(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.year}"
+
+    class Meta:
+        unique_together = (
+            "name",
+            "year",
+        )
+
+
+class Grade(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    grade = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.grade}, {self.person}, {self.course}"
